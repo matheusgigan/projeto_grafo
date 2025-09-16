b83f81d41c8baf8eecaf1a0127a0cdc670b35efc
matheusgigan <matheusvasconcelosaraujo@gmail.com>
2025-09-16 16:29:20 -0300

    Criando formularios e a lógica

--- grafo/urls.py
+++ grafo/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from grafo.views import index
+from django.urls import path, include
+from grafo.views import index, tela_inicial
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', tela_inicial, name='tela_inicial'),
+    path('index/', index, name='index'),
+
 ]
--- grafo/views.py
+++ grafo/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'grafo/index.html')
+    tamanho_vertices = None
+    nome_vertices =  None
+
+    if request.method == "POST":
+        if "numero_vertices" in request.POST:
+            numero_vertices = int(request.POST.get("numero_vertices"))
+            tamanho_vertices = range(numero_vertices)
+            return render(request, "grafo/index.html", {"tamanho_vertices": tamanho_vertices})
+
+        elif "nome_vertice" in request.POST:
+            nome_vertices = request.POST.getlist("nome_vertice")
+            tamanho_vertices = range(len(nome_vertices))
+            context = {
+                "tamanho_vertices": tamanho_vertices,
+                "nome_vertices": nome_vertices
+            }
+            return render(request, "grafo/index.html", context)
+
+    return render(request, "grafo/index.html")
+
+def tela_inicial(request):
+    return render(request, 'grafo/tela-inicial.html')
